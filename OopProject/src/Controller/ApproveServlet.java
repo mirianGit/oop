62c0ae061d03ae7e7e6d66b8171680fb912d3ce6
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2013-06-09 01:50:48 +0400

    approve-is servlet approves uketebs im kerdzs romelzec daclickda

--- OopProject/src/Controller/ApproveServlet.java
+++ OopProject/src/Controller/ApproveServlet.java
@@ -1,16 +1,20 @@
 package Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Model.Dish;
+
 /**
  * Servlet implementation class approveServlet
  */
-@WebServlet("/approveServlet")
+@WebServlet("/ApproveServlet")
 public class ApproveServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +30,11 @@ public class ApproveServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		int id=Integer.parseInt(request.getParameter("hidden"));
+		Dish d= Dish.getDish(Dish.getName(id));
+		d.approve();
+		RequestDispatcher dispatch = request.getRequestDispatcher("userServlet");
+		dispatch.forward(request, response);
 	}
 
 	/**
