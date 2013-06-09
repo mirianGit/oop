bb570bd31988d4cca5c9a6870967803df3c2660b
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2013-06-09 17:51:17 +0400

    useris waahlis servleti

---
+++ OopProject/src/Controller/DeleteUserServlet.java
@@ -0,0 +1,47 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.User;
+
+/**
+ * Servlet implementation class DeleteUserServlet
+ */
+@WebServlet("/DeleteUserServlet")
+public class DeleteUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id=Integer.parseInt(request.getParameter("hidden"));
+		User.deleteUser(id);
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("allUsersServlet");
+		dispatch.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
